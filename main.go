9bde8abe752e2518fd180256c90e2cff6f918bdc
DimMilios <elles1992@yahoo.gr>
2022-04-21 20:13:21 +0300

    feat: implement some simple cli commands

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"os"
+	"strings"
 
-	"github.com/go-do/todo"
+	todo "github.com/go-do/todo"
 	"github.com/urfave/cli/v2"
 )
 
@@ -13,27 +15,56 @@ func init() {
 }
 
 func main() {
+	var tag, value string
+
 	app := &cli.App{
-		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:     "create",
-				Aliases:  []string{"c"},
-				Usage:    "`Todo` value based on todo.txt format",
-				Required: true,
+		Commands: []*cli.Command{{
+			Name:    "create",
+			Aliases: []string{"c"},
+			Usage:   "`Todo` value based on todo.txt format",
+			Action: func(c *cli.Context) error {
+				if c.Args().Len() > 0 {
+					t, err := todo.Parse(c.Args().First())
+					log.Println(t.Format())
+					if err != nil {
+						return err
+					}
+
+					todo.AddToFile(t, nil)
+				} else {
+					log.Println("Couldn't parse todo.")
+				}
+				return nil
 			},
 		},
-		Action: func(c *cli.Context) error {
-			if c.String("create") != "" {
-				todo, err := todo.Parse(c.String("create"))
-				log.Println(todo)
-				if err != nil {
-					return err
-				}
+			{
+				Name:  "show",
+				Usage: "Show all saved todos",
+				Flags: []cli.Flag{
+					&cli.StringFlag{Name: "tag", Aliases: []string{"t"}, Destination: &tag},
+					&cli.StringFlag{Name: "value", Aliases: []string{"v"}, Destination: &value},
+				},
+				Action: func(c *cli.Context) error {
+					if len(tag) > 0 {
+						log.Println(tag)
+						if len(value) == 0 {
+							return errors.New("you have to provide a value when passing in a tag")
+						}
+						log.Println(value)
 
-			} else {
-				log.Println("Couldn't parse todo.")
-			}
-			return nil
+						switch strings.ToLower(tag) {
+						case strings.ToLower(todo.Project.String()):
+							todo.PrintByTag(todo.Project, value)
+						case strings.ToLower(todo.Context.String()):
+						case strings.ToLower(todo.KeyValue.String()):
+						default:
+							return errors.New("viable tag values are one of project, context or keyvalue")
+						}
+					}
+					todo.PrintAll(nil)
+					return nil
+				},
+			},
 		},
 	}
 
